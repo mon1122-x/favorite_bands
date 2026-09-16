8237d24fc1f085a630bd099347ea16e30887b414
chanutnunMon <rgdrgdrfse@gmail.com>
2026-09-16 14:43:36 +0700

    clean up folder structure and fix app router

--- .next/types/app/favorite_bands/page.ts
+++ .next/types/app/favorite_bands/page.ts
@@ -1,8 +1,8 @@
 // File: C:\favorite_bands\src\app\favorite_bands\page.tsx
-import * as entry from '../../../../src/app/favorite_bands/page.js'
+import * as entry from '../../../../src/app/page.jsx'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../src/app/favorite_bands/page.js')
+type TEntry = typeof import('../../../../src/app/page.jsx')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
